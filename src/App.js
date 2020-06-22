b83c6e06866ada940ce694949ec764cdc8540424
Amarjot singh <amarjotsingh90@yahoo.com>
2020-06-21 19:36:23 -0700

    hooks and props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <p>this is me</p>
-      </div>
-    );
+
+  state = {
+    person:[
+      { name : "Amar", age : 28, gender :"male", hobby: "cricket"},
+      { name : "Rozal", age : 27, gender :"male", hobby: "soccer"},
+      { name : "Stephanie", age : 26, gender :"female", hobby: "frizbee"},
+    ]
+  };
+
+  changeNameHandler = () => {
+    this.setState({
+      person:[
+        { name : "Amarjot", age : 28, gender :"male", hobby: "cricket"},
+        { name : "Rozal", age : 27, gender :"male", hobby: "soccer"},
+        { name : "Stephanie", age : 27, gender :"female", hobby: "friz"},
+      ] 
+    });
+  };
+
+      render() {
+        return (
+          <div className="App">
+            <button onClick = {this.changeNameHandler}>Click me</button>
+            <Person name = {this.state.person[0].name} age= {this.state.person[0].age} gender = {this.state.person[0].gender}>{this.state.person[0].hobby}</Person>
+            <Person name = {this.state.person[1].name} age= {this.state.person[1].age} gender = {this.state.person[1].gender}>{this.state.person[1].hobby}</Person>
+            <Person name = {this.state.person[2].name} age= {this.state.person[2].age} gender = {this.state.person[2].gender}>{this.state.person[2].hobby}</Person>
+          </div>
+        );
   }
 }
 
